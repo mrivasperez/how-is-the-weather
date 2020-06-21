9eb7412df3c55dfc04d2b29a3c69a5e0c815f74f
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-06-20 23:02:17 -0700

    added npm libraries

--- src/app.js
+++ src/app.js
@@ -12,9 +12,20 @@ const partialsPath = path.join(__dirname, '../views/partials');
 
 // Setup handlebars engine
 app.set('view engine', 'hbs');
-hbs.registerPartials('partialsPath');
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirPath));
 
 // Setup what is served based on url visisted:
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'how is the weather?'
+    });
+});
+
+
+// set port to 3000
+app.listen(3000, () => {
+    console.log('Connected at localhost:3000')
+});
