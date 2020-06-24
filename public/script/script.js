2b9465ff5e52a31c5a87c7ae761192b0daa03146
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-06-23 21:11:42 -0700

    created user interface interaction

--- public/script/script.js
+++ public/script/script.js
@@ -1,4 +1,11 @@
-const searchBox = document.getElementById('searchText');
+const searchBox = document.getElementById('searchText'),
+locationName = document.getElementById('locationName'),
+forecastDescription = document.getElementById('summary'),
+dataColOne = document.getElementById('col-one'),
+dataColTwo = document.getElementById('col-two'),
+theHeader = document.getElementById('theHeader');
+
+
 
 
 console.log('Connected to client-side script!');
@@ -25,7 +32,16 @@ const getForecast = (searchValue) => {
 const displayForecast = (locationData, forecastData) => {
     console.log(locationData);
     console.log(forecastData);
-    searchBox.value = ''
+    searchBox.value = '';
+    // if the weather is sunny, tell the user to go inside
+    theHeader.style.display = 'none';
+    console.log(forecastData.weather_descriptions[0]);
+    locationName.innerText = `The weather in ${locationData.name}`;
+    forecastDescription.innerText = `In ${locationData.name} it is currently ${forecastData.weather_descriptions[0].toLowerCase()}. Here are additional details to help you decide on whether you want to go outside or not:`;
+    dataColOne.innerHTML = {`
+    <p>
+    `}
+    // such and such .innerhtml for columns
 }
 
 
